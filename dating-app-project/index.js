c7f18f4fc4cb4a82bc514528148ca97e380e507a
irisvanollefen <irisvanollefen@gmail.com>
2020-05-20 09:18:11 +0200

    Updated dating app

--- dating-app-project/index.js
+++ dating-app-project/index.js
@@ -8,19 +8,56 @@ const fs = require("fs");
 const path = require("path");
 const Handlebars = require("handlebars");
 
-const template = Handlebars.compile(viewsFile);
+// creating a template with my header inside to make it in to a partial so I can call it on multiple pages
+const headerTemplate = fs.readFileSync(
+  path.join(__dirname, "views/header.html"),
+  "utf-8"
+);
+
+// creating a variable that will compile the header template
+const renderHeader = Handlebars.compile(headerTemplate);
+
+// registering the header partial
+Handlebars.registerPartial("myPartial", renderHeader);
+
+// this is where my chat overview page is coming from, which I also put in a template so it easy to use again
+const chatOverviewTemplate = fs.readFileSync(
+  path.join(__dirname, "views/chat-overview-page.html"),
+  "utf-8"
+);
+
+// rendering the chat overview template and compiling that using handlebars
+const renderChatOverViewTemplate = Handlebars.compile(chatOverviewTemplate);
+
+// registering the header partial
+Handlebars.registerPartial("myPartial", renderHeader);
+
+// this is where my chat overview page is coming from, which I also put in a template so it easy to use again
+const newMatchTemplate = fs.readFileSync(
+  path.join(__dirname, "views/new-match.html"),
+  "utf-8"
+);
+
+// rendering the chat overview template and compiling that using handlebars
+const renderNewMatchTemplate = Handlebars.compile(newMatchTemplate);
 
 // creating routes
 app.use(express.static("public"));
 
+// creating a route which will deliver my chat overview page which will consist the header
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/views/chat-overview-page.html");
+  const newTemplate = renderChatOverViewTemplate();
+  res.send(newTemplate);
 });
 
 app.get("/new-match", (req, res) => {
-  res.sendFile(__dirname + "/views/new-match.html");
+  const newTemplate = renderNewMatchTemplate();
+  res.send(newTemplate);
 });
 
+// // creating a partial for the header and footer
+
+// on which port it will listen
 const server = app.listen(8000, () => {
   console.log(
     "Your dating app is now running at http://localhost:" +
