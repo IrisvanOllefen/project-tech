84c6130628635880fa5ccf66d6096083c897bb93
irisvanollefen <irisvanollefen@gmail.com>
2020-05-23 14:53:33 +0200

    updates comments

--- exercise-input/index.js
+++ exercise-input/index.js
@@ -5,11 +5,13 @@ const fs = require("fs");
 const path = require("path");
 // using handlebars as my templating engine
 const Handlebars = require("handlebars");
+// using bodyParser to parse input from my form
 const bodyParser = require("body-parser");
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
 
+// a function which makes the read file things dynamic so I don't have to do the same thing multiple times
 function readViewContent(fileName) {
   const viewFileContent = fs.readFileSync(
     path.join(__dirname, `views/${fileName}.html`),
@@ -18,22 +20,26 @@ function readViewContent(fileName) {
   return viewFileContent;
 }
 
+// this function takes the function above and returns it in to handlebars
 function renderViewContent(fileName) {
   const content = readViewContent(fileName);
   return Handlebars.compile(content);
 }
 
-// rendering the chat overview template and compiling that using handlebars
+// here i am rendering the two pages I need
 const renderIndex = renderViewContent("index");
 const renderThanks = renderViewContent("thanks");
 
+// making the two routes
 app.get("/", (req, res) => {
   const newTemplate = renderIndex();
   res.send(newTemplate);
 });
 
 app.post("/thanks", (req, res) => {
+  // to check my console for the result
   console.log(req.body);
+  // req.body returnes the parsed body items thanks to body.parser
   const newTemplate = renderThanks({ body: req.body });
   res.send(newTemplate);
 });
